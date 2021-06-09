55a4b8dda0dac5b79a84d206314d636b69cb16ff
Alexandr Ogurtsov <myonlygamess@gmail.com>
2021-06-09 20:20:19 +0300

    Init tdd todos

--- test/LifeCycleService.test.js
+++ test/LifeCycleService.test.js
@@ -1,3 +1,11 @@
 describe('Life cycle cell service', () => {
-    it.todo('asd')
+    it.todo('Without any change. Matrix 4x4');
+
+    it.todo('Simple line changes direction. Matrix 5x5');
+
+    it.todo('Moves to the right bottom corner. Matrix 6x6');
+
+    it.todo('Simple fractal changes. Matrix 6x6');
+
+    it.todo('Complex big fractal. Matrix 17x17');
 });
