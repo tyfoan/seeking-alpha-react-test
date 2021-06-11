20bf9ad84d98254b9d59e8614c1c85900d1b987b
Alexandr Ogurtsov <myonlygamess@gmail.com>
2021-06-11 19:27:51 +0300

    Before sending

--- src/services/LifeCycleService.ts
+++ src/services/LifeCycleService.ts
@@ -1,4 +1,4 @@
-const NEIGHBOURS_X_Y: number[][] = [
+const NEIGHBOURS_COORDINATES: number[][] = [
     [-1, -1], [-1, 0], [-1, 1], [0, 1],
     [1, 1], [1, 0], [1, -1], [0, -1],
 ];
@@ -12,74 +12,76 @@ class LifeCycleService {
     }
 
     getNextGeneration(): number[][] {
-        this.generation = this._makeNextGeneration();
+        this.generation = this.makeNextGeneration();
         this.nextGeneration = [];
+
         return this.generation;
     }
 
-    get _width(): number {
+    private get width(): number {
         return this.generation.length;
     }
 
-    get _height(): number {
+    private get height(): number {
         return this.generation[0].length
     }
 
-    _makeNextGeneration(): number[][] {
-        for (let x = 0; x < this._width; x++) {
+    private makeNextGeneration(): number[][] {
+        for (let x = 0; x < this.width; x++) {
             this.nextGeneration.push([]);
-            for (let y = 0; y < this._height; y++) {
-                this.nextGeneration[x].push(this._makeNextGenerationCell(x, y));
+            for (let y = 0; y < this.height; y++) {
+                this.nextGeneration[x].push(this.makeNextGenerationCell(x, y));
             }
         }
 
         return this.nextGeneration;
     }
 
-    _makeNextGenerationCell(cellX: number, cellY: number): number {
+    private makeNextGenerationCell(cellX: number, cellY: number): number {
         const cell = this.generation[cellX][cellY];
-        const neighbours = this._getCellNeighbours(cellX, cellY);
+        const neighbours = this.getCellNeighbours(cellX, cellY);
 
-        return this._chooseCellDestiny(cell, neighbours);
+        return this.chooseCellDestiny(cell, neighbours);
     }
 
-    _getCellNeighbours(cellX: number, cellY: number): number[] {
-        return NEIGHBOURS_X_Y.map(([neighbourX, neighbourY]) => {
-            const [newX, newY] = this._makeInfiniteStep(cellX + neighbourX, cellY + neighbourY);
+    private getCellNeighbours(cellX: number, cellY: number): number[] {
+        return NEIGHBOURS_COORDINATES.map(([neighbourX, neighbourY]) => {
+            const [newX, newY] = this.makeInfiniteStep(cellX + neighbourX, cellY + neighbourY);
+
             return this.generation[newX][newY];
         })
     }
 
-    _makeInfiniteStep(x: number, y: number): number[] {
-        const correctDeltaX = this._updateIfOutside(x, this._width - 1);
-        const correctDeltaY = this._updateIfOutside(y, this._height - 1);
+    private makeInfiniteStep(x: number, y: number): number[] {
+        const correctDeltaX = this.updateIfOutside(x, this.width - 1);
+        const correctDeltaY = this.updateIfOutside(y, this.height - 1);
 
         return [correctDeltaX, correctDeltaY];
     }
 
-    _updateIfOutside(coordinate: number, size: number): number {
-        if(coordinate >= 0 && coordinate <= size) {
+    private updateIfOutside(coordinate: number, size: number): number {
+        if (coordinate >= 0 && coordinate <= size) {
             return coordinate;
         }
 
         return coordinate > size ? 0 : size
     }
 
-    _chooseCellDestiny(cell: number, neighbours: number[]): 0 | 1 {
-        const aliveNeghbours = this._sumAlive(neighbours);
+    private chooseCellDestiny(cell: number, neighbours: number[]): number {
+        const aliveNeighbours = this.sumAlive(neighbours);
 
-        if(this._isDead(cell)) {
-            if(this._reproduction(aliveNeghbours)) {
+        if (this.isDead(cell)) {
+            if (this.reproduction(aliveNeighbours)) {
                return 1;
             }
         } else {
-            if(this._underpopulation(aliveNeghbours)) {
+            if (this.underpopulation(aliveNeighbours)) {
                 return 0;
             }
-            if(this._generation(aliveNeghbours)) {
+            if (this.isNextGeneration(aliveNeighbours)) {
                 return 1;
             }
-            if(this._overcrowding(aliveNeghbours)) {
+            if (this.overcrowding(aliveNeighbours)) {
                 return 0;
             }
         }
@@ -87,32 +89,28 @@ class LifeCycleService {
         return 0;
     }
 
-    _sumAlive(neghbours: number[]): number {
-        return neghbours.reduce((a, b) => a + b, 0);
+    private sumAlive(neighbours: number[]): number {
+        return neighbours.reduce((a, b) => a + b, 0);
     }
 
-    _isDead(cell: number): boolean {
+    private isDead(cell: number): boolean {
         return cell === 0
     }
 
-    _isAlive(cell: number): boolean {
-        return cell === 1
+    private underpopulation(aliveNeighbours: number): boolean {
+        return aliveNeighbours < 2;
     }
 
-    _underpopulation(aliveNeghbours: number) {
-        return aliveNeghbours < 2;
+    private isNextGeneration(aliveNeighbours: number): boolean {
+        return aliveNeighbours === 2 || aliveNeighbours === 3;
     }
 
-    _generation(aliveNeghbours: number) {
-        return aliveNeghbours === 2 || aliveNeghbours === 3;
+    private overcrowding(aliveNeighbours: number): boolean {
+        return aliveNeighbours > 3
     }
 
-    _overcrowding(aliveNeghbours: number) {
-        return aliveNeghbours > 3
-    }
-
-    _reproduction(aliveNeghbours: number): boolean {
-        return aliveNeghbours === 3;
+    private reproduction(aliveNeighbours: number): boolean {
+        return aliveNeighbours === 3;
     }
 }
 
